0f1cfc9fcfe1da61bad522272826d48d6b3b69a2
Jansen Felipe <jansen.felipe@gmail.com>
2015-05-27 01:12:24 -0300

    Add trait

--- app/Cliente.php
+++ app/Cliente.php
@@ -1,8 +1,11 @@
 <?php namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Laraerp\Ordination\OrdinationTrait;
 
 class Cliente extends Model {
+
+    use OrdinationTrait;
 
     protected $fillable = ['documento', 'nome', 'razao_social'];
 
